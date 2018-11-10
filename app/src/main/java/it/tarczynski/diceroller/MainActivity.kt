1ea2e99f03b83453048aea47e9132f5c84721667
Tomasz Tarczyński <tarczynskitomek@gmail.com>
2018-11-10 18:15:17 +0100

    Add basic views and activity for DiceRoller app

--- app/src/main/java/it/tarczynski/diceroller/MainActivity.kt
+++ app/src/main/java/it/tarczynski/diceroller/MainActivity.kt
@@ -1,12 +1,36 @@
 package it.tarczynski.diceroller
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.ImageView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    private fun rollDice() {
+        val resource = when (Random.nextInt(diceLowerBound, diceUpperBoundExclusive)) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        diceImage.setImageResource(resource)
+    }
+
+    companion object DiceBounds {
+        const val diceLowerBound = 1
+        const val diceUpperBoundExclusive = 7
     }
 }
